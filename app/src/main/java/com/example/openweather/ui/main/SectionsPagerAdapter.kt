4796b363d931c3e9abbed57194d37afd603a3fa9
Karam Ibrahim <karamzeroo@gmail.com>
2022-03-27 00:55:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/openweather/ui/main/SectionsPagerAdapter.kt
@@ -0,0 +1,40 @@
+package com.example.openweather.ui.main
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.openweather.R
+import com.example.openweather.TodayFragment
+import com.example.openweather.TomorrowFragment
+
+private val TAB_TITLES = arrayOf(
+    R.string.tab_text_1,
+    R.string.tab_text_2
+)
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        var fragment : Fragment? = null
+        when(position){
+            0 ->{fragment = TodayFragment()}
+            1 ->{fragment = TomorrowFragment()}
+        }
+        return fragment!!
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return context.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        // Show 2 total pages.
+        return 2
+    }
+}
