a8f08e10ca760dc88eb680aa0d03decd49038440
Karam Ibrahim <karamzeroo@gmail.com>
2022-03-28 03:04:17 +0200

    new DrawerLayout

--- app/src/main/java/com/example/openweather/MainActivity.kt
+++ app/src/main/java/com/example/openweather/MainActivity.kt
@@ -8,18 +8,28 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.openweather.ui.main.SectionsPagerAdapter
 import com.example.openweather.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val drawerLayout: DrawerLayout = binding.myDrawer
+
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
         val viewPager: ViewPager = binding.viewPager
@@ -31,6 +41,12 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+            drawerLayout.open()
         }
+
+        setSupportActionBar(binding.toolbar)
+
+
+       // drawerLayout.addDrawerListener()
     }
 }
